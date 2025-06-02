3821ecf6da27feac42dfa76a855968c0389dd98a
Moutaz Mahmoud <ac154958@avcol.school.nz>
2025-06-03 11:42:42 +1200

    Ive added the Initializer havent finished it just yet but ive finished the one for Customers, and ive also fixed some other things here and there

---
+++ TechHaven/Areas/Identity/Data/DBInitializer.cs
@@ -0,0 +1,38 @@
+﻿using TechHaven.Models;
+
+namespace TechHaven.Areas.Identity.Data
+{
+    public class DBInitializer
+    {
+        internal static void Initialize(TechHavenContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Customer.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var customers = new Customer[]
+            {
+                new Customer { FirstName = "Alex", LastName = "Benjaman", Email = "Alex31@gmail.com" },
+                new Customer { FirstName = "Bob", LastName = "The Builder", Email = "Bob123@gmail.com" },
+                new Customer { FirstName = "Michael", LastName = "Joradn", Email = "Michael531@gmail.com" },
+                new Customer { FirstName = "Mike", LastName = "Tyson", Email = "connorm1@gmail.com" },
+                new Customer { FirstName = "Max", LastName = "farewel", Email = "connorm1@gmail.com" },
+                new Customer { FirstName = "Jhon", LastName = "Jones", Email = "connorm1@gmail.com" },
+                new Customer { FirstName = "David", LastName = "Hallen", Email = "connorm1@gmail.com" },
+                new Customer { FirstName = "William", LastName = "Finn", Email = "William@gmail.com" },
+                new Customer { FirstName = "Joe", LastName = "Rogan", Email = "Joe@gmail.com" },
+                new Customer { FirstName = "Khabib", LastName = "Nurmagomedov", Email = "Khabib@gmail.com" },
+            };
+            foreach (Customer c in customers)
+            {
+                context.Customer.Add(c);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
