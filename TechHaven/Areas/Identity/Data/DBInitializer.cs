6acd9c02a2351f5cb02ed18d5e5dc29a3ea37e20
Moutaz Mahmoud <ac154958@avcol.school.nz>
2025-06-10 10:46:29 +1200

    Just adding the DB Initializer

--- TechHaven/Areas/Identity/Data/DBInitializer.cs
+++ TechHaven/Areas/Identity/Data/DBInitializer.cs
@@ -26,15 +26,15 @@ namespace TechHaven.Areas.Identity.Data
             {
 
                 new Customer { FirstName = "Alex", LastName = "Benjaman", Email = "Alex31@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand" },
-                new Customer { FirstName = "Bob", LastName = "The Builder", Email = "Bob123@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand" },
-                new Customer {FirstName = "Michael", LastName = "Joradn", Email = "Michael531@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "Mike", LastName = "Tyson", Email = "Mike@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "Max", LastName = "farewel", Email = "Max@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "Jhon", LastName = "Jones", Email = "Jhon@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "David", LastName = "Hallen", Email = "David@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "William", LastName = "Finn", Email = "William@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "Joe", LastName = "Rogan", Email = "Joe@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
-                new Customer {FirstName = "Khabib", LastName = "Nurmagomedov", Email = "Khabib@gmail.com", Phone = 0220398787, Address = "72 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
+                new Customer { FirstName = "Bob", LastName = "The Builder", Email = "Bob123@gmail.com", Phone = 0227291560, Address = "Parnell, Auckland 1010, New Zealand" },
+                new Customer {FirstName = "Michael", LastName = "Joradn", Email = "Michael531@gmail.com", Phone = 0227206418,  Address = "22 Princes Street, Auckland CBD, Auckland 1010, New Zealand"},
+                new Customer {FirstName = "Mike", LastName = "Tyson", Email = "Mike@gmail.com", Phone = 0212046835, Address = "286 Mount Wellington Highway, Mount Wellington, Auckland 1060, New Zealand"},
+                new Customer {FirstName = "Max", LastName = "farewel", Email = "Max@gmail.com", Phone = 0221839472, Address = "Motions Road, Western Springs, Auckland 1022, New Zealand"},
+                new Customer {FirstName = "Jhon", LastName = "Jones", Email = "Jhon@gmail.com", Phone = 0221768503, Address = "Wellesley Street East, Auckland CBD, Auckland 1010, New Zealand"},
+                new Customer {FirstName = "David", LastName = "Hallen", Email = "David@gmail.com", Phone = 0219204176, Address = "Reimers Avenue, Kingsland, Auckland 1024, New Zealand"},
+                new Customer {FirstName = "William", LastName = "Finn", Email = "William@gmail.com", Phone = 0221395842, Address = "Tamaki Drive, Mission Bay, Auckland 1071, New Zealand"},
+                new Customer {FirstName = "Joe", LastName = "Rogan", Email = "Joe@gmail.com", Phone = 0222057936, Address = "2 Park Road, Grafton, Auckland 1023, New Zealand"},
+                new Customer {FirstName = "Khabib", LastName = "Nurmagomedov", Email = "Khabib@gmail.com", Phone = 0221483705, Address = "210 Victoria Street West, Auckland CBD, Auckland 1010, New Zealand"},
 
             };
 
@@ -75,16 +75,16 @@ namespace TechHaven.Areas.Identity.Data
 
             var Orders = new Order[]
             {
-                new Order { OrderDate = 09/5/23, TottalPrice = 1300 },
-                new Order { OrderDate = 02/7/23, TottalPrice = 1250 },
-                new Order { OrderDate = 05/3/23, TottalPrice = 970 },
-                new Order { OrderDate = 04/5/24, TottalPrice = 1000 },
-                new Order { OrderDate = 08/5/24, TottalPrice = 999 },
-                new Order { OrderDate = 26/6/24, TottalPrice = 1370},
-                new Order { OrderDate = 17/8/24, TottalPrice = 2700},
-                new Order { OrderDate = 18/11/24, TottalPrice = 2000},
-                new Order { OrderDate = 29/12/24, TottalPrice = 1500},
-                new Order { OrderDate = 30/12/24, TottalPrice= 1200},
+                new Order { OrderDate = 09/5/23, TottalPrice = 1300, ProductName = "HP Laptop"},
+                new Order { OrderDate = 02/7/23, TottalPrice = 1250, ProductName = "Iphone 16 pro max"},
+                new Order { OrderDate = 05/3/23, TottalPrice = 700, ProductName = "Acer Chromebook"},
+                new Order { OrderDate = 04/5/24, TottalPrice = 1000, ProductName = "Mac book"},
+                new Order { OrderDate = 08/5/24, TottalPrice = 999, ProductName = "Ipad Pro"},
+                new Order { OrderDate = 26/6/24, TottalPrice = 1370, ProductName = "PC"},
+                new Order { OrderDate = 17/8/24, TottalPrice = 2700, ProductName = "samsong odessy Monitor"},
+                new Order { OrderDate = 18/11/24, TottalPrice = 2000, ProductName = "Samsung Tv"},
+                new Order { OrderDate = 29/12/24, TottalPrice = 1500, ProductName = "Gamming Laptop"},
+                new Order { OrderDate = 30/12/24, TottalPrice= 1200, ProductName = "HP Laptop"},
             };
             foreach (Order o in Orders)
             {
