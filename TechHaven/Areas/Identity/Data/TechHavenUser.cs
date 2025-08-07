ff2304c571217b2abae900a3821a8f473e867480
ac154958 <ac154958@WKS-GM0Z64FN>
2025-08-07 15:14:38 +1200

    added a couple of new things

---
+++ TechHaven/Areas/Identity/Data/TechHavenUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+namespace TechHaven.Areas.Identity.Data
+{
+    public class TechHavenUser: IdentityUser
+    {
+    }
+}
